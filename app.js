39e38f60171f36e68458b2ac35e599ba75a10dff
ZH <killerwhokillu@gmail.com>
2018-07-23 11:40:18 +0800

    quite a lot of shit

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var paypal = require('paypal-rest-sdk');
+var stripe = require("stripe")("sk_test_mjPvQTYjNImEEt3PTQk3KpbZ");
+var exphbs = require('express-handlebars');
+
 
 paypal.configure({
     'mode': 'sandbox', //sandbox or live
     'client_id': 'AXLNohjxp86UfQQ3DD11Ah6kMQ8i3ZTuprLYshS8nc_OhS8M1Ot1W57jbwjz140-3pRA6KhbAgq5_AcD',
     'client_secret': 'EC8xYilzzi9A5ndaAOIGMEOv_VtMX-gcdadzShjoP4HmdioYG0tFJOq9U7WGAyxze6cj41A84a8JYQmC',
   });
+
+  //middleware
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
 
 // Import controllers
 var home = require('./controllers/home');
@@ -34,6 +42,8 @@ app.use(cookieParser());
 // View engine ejs
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
+
+
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -61,6 +71,11 @@ app.get("/transactions", transaction.showAll);
 app.post("/transactions", transaction.create);
 app.get("/transactions/:transaction_id", transaction.showDetails);
 app.post("/transactions/:transaction_id", transaction.testpay);
+
+
+//Charge route
+app.post("/charge/:transaction_id", checkoutcard.charge);
+
 
 // Listening
 var server = app.listen(3000, () => {
